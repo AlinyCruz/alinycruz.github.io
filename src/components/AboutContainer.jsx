8a5019cf8646d35399c22a46e7dde4e201250a1c
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-10-09 17:26:41 -0300

    Atualizando sobre

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,10 +2,10 @@ const AboutContainer = () => {
   return <section className="about-container">
     <h2>Sobre</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, aliquam? Nihil praesentium est, eaque ducimus temporibus repellat omnis necessitatibus sunt voluptas veritatis eveniet assumenda sit iure eum aliquid fugiat! Quis!
+      Em construção....
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus pariatur, nisi nulla voluptates nemo aliquam quisquam dolorem architecto aspernatur inventore culpa eum consequatur tenetur dolorum, eos praesentium quaerat beatae cupiditate.
+      ......
     </p>
   </section>
 }
