d60f808e2897ed9b41d8c6809aa4b4b20aaf4b89
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-10-10 16:59:30 -0300

    Finalizando portfolio

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,10 +2,10 @@ const AboutContainer = () => {
   return <section className="about-container">
     <h2>Sobre</h2>
     <p>
-      Em construção....
+      Olá, meu nome é Aline, sou formada em Desenvolvimento Web. Estou em busca de minha primeira oportunidade para fazer parte do mundo da programação. No momento em que comecei a estudar a programação, fiquei encantada com a capacidade de criar experiências digitais incríveis que podem impactar positivamente a vida das pessoas. Desde de criança já tinha uma paixão em tecnologia e de transformar ideias criativas em soluções funcionais e interativas. Estou animada em dar os primeiros passos em minha carreira como desenvolvedora e estou em busca de uma oportunidade para contribuir em projetos empolgantes e aprender com profissionais experientes. Estou disposta a abraçar desafios e crescer como parte de uma equipe talentosa. Embora seja nova na área, estou empenhada em aprender e crescer constantemente.
     </p>
     <p>
-      ......
+      Meu objetivo é me tornar uma desenvolvedora de sucesso, criando experiências digitais impactantes e inovadoras. Estou ansiosa para embarcar nesta jornada emocionante e construir um futuro brilhante no desenvolvimento web!
     </p>
   </section>
 }
--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,7 +2,7 @@ const ProjectsContainer = () => {
   return <section className="projects-container">
     <h2>Projetos</h2>
     <p>
-      No decorrer do curso, desenvolvi diversos projetos FrontEnd / BackEnd, individuais e também em equipe. Grande parte esta disponível em meu repositório do GitHub! Confira!
+      No decorrer do curso, desenvolvi diversos projetos FrontEnd e BackEnd, sendo eles, individuais e também em equipe. Grande parte esta disponível em meu repositório do GitHub! Confira!
     </p>
     <a href="https://github.com/AlinyCruz" className="btn" target="_blank" rel="noreferrer">
       Ver projetos
