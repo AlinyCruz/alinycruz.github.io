9c44041bb8f353da3c1d0a42bb35f675188a5716
AlinyCruz <aliny.cruz@yahoo.com.br>
2025-05-28 18:51:10 -0300

    Atualiza descricao sobre

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,10 +2,16 @@ const AboutContainer = () => {
   return <section className="about-container">
     <h2>Sobre</h2>
     <p>
-      Olá, meu nome é Aline, sou formada em Desenvolvimento Web. Estou em busca de minha primeira oportunidade para fazer parte do mundo da programação. No momento em que comecei a estudar a programação, fiquei encantada com a capacidade de criar experiências digitais incríveis que podem impactar positivamente a vida das pessoas. Desde de criança já tinha uma paixão em tecnologia e de transformar ideias criativas em soluções funcionais e interativas. Estou animada em dar os primeiros passos em minha carreira como desenvolvedora e estou em busca de uma oportunidade para contribuir em projetos empolgantes e aprender com profissionais experientes. Estou disposta a abraçar desafios e crescer como parte de uma equipe talentosa. Embora seja nova na área, estou empenhada em aprender e crescer constantemente.
+      Olá, meu nome é Aline,
+      Iniciei minha carreira profissional em 2008 atuando na área administrativa. No entanto, desde criança, sempre fui fascinada por tecnologia. Esse interesse me motivou a estudar mais a tecnologia. Em 2023 me formei em Desenvolvimento Full-Stack pela Trybe, após um ano e meio de intenso aprendizado.
+
+      Atualmente, estou cursando Análise e Desenvolvimento de Sistemas para aprofundar meus conhecimentos. Sou uma entusiasta da aprendizagem contínua, sempre buscando novas oportunidades para crescer e me desenvolver profissionalmente na área tecnologica.
+
+      Estou determinada a me tornar uma profissional de excelência!
     </p>
     <p>
-      Meu objetivo é me tornar uma desenvolvedora de sucesso, criando experiências digitais impactantes e inovadoras. Estou ansiosa para embarcar nesta jornada emocionante e construir um futuro brilhante no desenvolvimento web!
+      Meu objetivo é me tornar uma desenvolvedora de sucesso, criando experiências digitais impactantes e inovadoras.
+      Vou embarcar nesta jornada emocionante e construir um futuro brilhante no desenvolvimento web!
     </p>
   </section>
 }
